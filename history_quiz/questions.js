c434f3a9cd5e7ce0d61c0096c63c2efed4ab10bf
Otto Stegmaier <ottos@ottos-macbookpro.roam.corp.google.com>
2018-01-19 17:51:10 -0800

    Moved questions to own file.

---
+++ history_quiz/questions.js
@@ -0,0 +1,21 @@
+const QUESTIONS = [
+  {
+    text: 'According to Greek mythology, who was the god of wine?',
+    answers: ['Dionysos','Baccus','Adonis','Venus',],
+    correctAnswer: 'Dionysos',
+  },
+  {
+    text: 'Ruling for 64 years, which Queen was the longest-reigning British monarch before Queen Elizabeth II?',
+    answers: ['Queen Victoria','Queen Elizabeth I','Quen Anne','Queen Eleanor'],
+    correctAnswer: 'Queen Victoria',
+  },
+  ];
+
+
+// ['Guilder','Ducat','Monero','Dinero',]
+// ['Calvin Coolidge','Teddy Roosevelt','William Mckinley','Franklin Roosevelt',]
+// ['Tax Evasion','Murder','Grand Theft Auto','Bank Robbery',]
+// ['The Renaissance','The Guilded Age','The Victorian Era','The bronze age',]
+// ['Constantinople','Jerusalem','Athens','Mosul',]
+// ['Hostpital','Barn','Automobile','Drought',]
+// ['Russia','China','Canada','Spain',]
