e52311c2a84236e640ca63d64e5232b928ac3bf6
Otto Stegmaier <ottos@ottos-macbookpro.roam.corp.google.com>
2018-01-25 10:50:13 -0800

    Modal display and background.

--- history_quiz/questions.js
+++ history_quiz/questions.js
@@ -4,46 +4,46 @@ const originalQuestions = [
     answers: ['Dionysus','Baccus','Adonis','Venus',],
     correctAnswer: 'Dionysus',
   },
-  {
-    text: 'Ruling for 64 years, which Queen was the longest-reigning British monarch before Queen Elizabeth II?',
-    answers: ['Queen Victoria','Queen Elizabeth I','Quen Anne','Queen Eleanor'],
-    correctAnswer: 'Queen Victoria',
-  },
-  {
-    text: 'Before the introduction of the euro, what was the name for the basic monetary unit used in the Netherlands?',
-    answers: ['Guilder','Ducat','Monero','Dinero',],
-    correctAnswer: 'Guilder',
-  },
-  {
-    text: 'Which U.S. president made the first presidential radio broadcast?',
-    answers: ['Calvin Coolidge','Teddy Roosevelt','William Mckinley','Franklin Roosevelt',],
-    correctAnswer: 'Calvin Coolidge',
-  },
-  {
-    text: 'Jimmy Carter was the first U.S. president born in a what?',
-    answers: ['Hospital','Barn','Automobile','Drought',],
-    correctAnswer: 'Hospital',
-  },
-  {
-    text: 'American mobster Al Capone was sentenced to 11 years in federal prison for what crime?',
-    answers:['Tax Evasion','Murder','Grand Theft Auto','Bank Robbery',],
-    correctAnswer: 'Tax Evasion',
-  },
-  {
-    text: 'The period of European history that lasted from the 14th to the the 17th century is known as what?',
-    answers:['The Renaissance','The Guilded Age','The Victorian Era','The bronze age',],
-    correctAnswer: 'The Renaissance',
-  },
-  {
-    text: 'What Byzantine city was renamed Istanbul after being captured by the Ottoman Empire?',
-    answers:['Constantinople','Jerusalem','Athens','Mosul',],
-    correctAnswer: 'Constantinople',
-  },
-  {
-    text: 'In 1867 the United States purchased Alaska from what country?',
-    answers:['Russia','China','Canada','Spain',],
-    correctAnswer: 'Russia',
-  },
+  // {
+  //   text: 'Ruling for 64 years, which Queen was the longest-reigning British monarch before Queen Elizabeth II?',
+  //   answers: ['Queen Victoria','Queen Elizabeth I','Quen Anne','Queen Eleanor'],
+  //   correctAnswer: 'Queen Victoria',
+  // },
+  // {
+  //   text: 'Before the introduction of the euro, what was the name for the basic monetary unit used in the Netherlands?',
+  //   answers: ['Guilder','Ducat','Monero','Dinero',],
+  //   correctAnswer: 'Guilder',
+  // },
+  // {
+  //   text: 'Which U.S. president made the first presidential radio broadcast?',
+  //   answers: ['Calvin Coolidge','Teddy Roosevelt','William Mckinley','Franklin Roosevelt',],
+  //   correctAnswer: 'Calvin Coolidge',
+  // },
+  // {
+  //   text: 'Jimmy Carter was the first U.S. president born in a what?',
+  //   answers: ['Hospital','Barn','Automobile','Drought',],
+  //   correctAnswer: 'Hospital',
+  // },
+  // {
+  //   text: 'American mobster Al Capone was sentenced to 11 years in federal prison for what crime?',
+  //   answers:['Tax Evasion','Murder','Grand Theft Auto','Bank Robbery',],
+  //   correctAnswer: 'Tax Evasion',
+  // },
+  // {
+  //   text: 'The period of European history that lasted from the 14th to the the 17th century is known as what?',
+  //   answers:['The Renaissance','The Guilded Age','The Victorian Era','The bronze age',],
+  //   correctAnswer: 'The Renaissance',
+  // },
+  // {
+  //   text: 'What Byzantine city was renamed Istanbul after being captured by the Ottoman Empire?',
+  //   answers:['Constantinople','Jerusalem','Athens','Mosul',],
+  //   correctAnswer: 'Constantinople',
+  // },
+  // {
+  //   text: 'In 1867 the United States purchased Alaska from what country?',
+  //   answers:['Russia','China','Canada','Spain',],
+  //   correctAnswer: 'Russia',
+  // },
 ];
 
 
